f8decc05b902afb5cffb07298c0b8183acfbcc19
chloe lai <chloe.jy.lai@gmail.com>
2018-09-02 10:20:03 +0800

    test

---
+++ src/main/java/com/clinic/anhe/ShiftRecord.java
@@ -0,0 +1,91 @@
+package com.clinic.anhe;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+//create_on Date not null,
+//nurse varchar(255) not null,
+//patient varchar(255) not null,
+//shift varchar(100),
+//day varchar(100) 
+@Entity
+@Table(name="shift_record")
+public class ShiftRecord {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(nullable=false)
+	private Integer sid;
+
+	@Column(name="create_on", nullable=false)
+    @Temporal(TemporalType.DATE)
+	@CreationTimestamp
+	private Date createAt;
+	
+	private String nurse;
+	
+	private String patient;
+	
+	public Integer getSid() {
+		return sid;
+	}
+
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+
+	public String getPatient() {
+		return patient;
+	}
+
+	public void setPatient(String patient) {
+		this.patient = patient;
+	}
+
+	private String shift;
+	
+	private String day;
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public String getNurse() {
+		return nurse;
+	}
+
+	public void setNurse(String nurse) {
+		this.nurse = nurse;
+	}
+
+	public String getShift() {
+		return shift;
+	}
+
+	public void setShift(String shift) {
+		this.shift = shift;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+	
+}
---
+++ src/main/java/com/clinic/anhe/ShiftRecordRepository.java
@@ -0,0 +1,7 @@
+package com.clinic.anhe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShiftRecordRepository extends JpaRepository<ShiftRecord, Long>{
+
+}
