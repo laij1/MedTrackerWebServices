fa293b37fcb81c8a8ce579dfdd074c1b13bf91b7
laij1 <chloe.jy.lai@gmail.com>
2019-04-21 10:03:30 +0800

    test rebasing 2

--- src/test/java/com/clinic/anhe/AnheApplicationTests.java
+++ src/test/java/com/clinic/anhe/AnheApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AnheApplicationTests {
 
-        //adding test comments.for rebasing test
+        //adding test comments. rebasing test. changes from github
 	@Test
 	public void contextLoads() {
 	}
