c7db2fcc06a8d21d01d2e7eac9f68a1bb5abb7d9
laij1 <chloe.jy.lai@gmail.com>
2019-04-21 10:59:05 +0800

    test conflict

--- src/test/java/com/clinic/anhe/AnheApplicationTests.java
+++ src/test/java/com/clinic/anhe/AnheApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AnheApplicationTests {
 
-        //adding test comments. rebasing test. changes from github
+        //adding test comments. rebasing test. github
 	@Test
 	public void contextLoads() {
 	}
